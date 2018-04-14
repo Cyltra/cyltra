ba56a5526e1f7458f36152be16d3c56967795741
Cyltra <38080633+Cyltra@users.noreply.github.com>
2018-04-14 16:41:26 +0200

    JS Change

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@
 
 $(document).ready(function(){
   // Add smooth scrolling to all links
-  $("a#smoothScroll").on('click', function(event) {
+  $("a#projects").on('click', function(event) {
 
     // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
